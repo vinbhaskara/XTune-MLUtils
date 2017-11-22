31597e219de6074d64a861725ffe536a1fea10b3
bsvineethiitg <bhaskaravineeth@gmail.com>
2017-11-22 17:55:31 +0000

    added target encoding

--- mltools.py
+++ mltools.py
@@ -16,6 +16,69 @@ from xtune import *
 from sklearn.metrics import roc_auc_score, log_loss
 
 
+# Dealing with categorical features Way 1
+def target_encode(trn_series=None, tst_series=None, target=None, in_samples_leaf=1,
+                  smoothing=1, noise_level=0):
+    """
+    Target Encoding for Categorical Features! An alternative to OneHot Encoding. But with a risk of GT leakage.
+    This converts Categorical to Numerical fields.
+    
+    Usage:
+    Example - Do Column by Column
+
+    for f in catcols:
+        train_df[f + "_new"], test_df[f + "_new"] = target_encode(trn_series=train_df[f],
+                                            tst_series=test_df[f],
+                                            target=train_df['target'],
+                                            min_samples_leaf=200,
+                                            smoothing=10,
+                                            noise_level=0) 
+    
+    
+    
+    Smoothing is computed like in the following paper by Daniele Micci-Barreca
+    https://kaggle2.blob.core.windows.net/forum-message-attachments/225952/7441/high%20cardinality%20categoricals.pdf
+    trn_series : training categorical feature as a pd.Series
+    tst_series : test categorical feature as a pd.Series
+    target : target data as a pd.Series
+    min_samples_leaf (int) : minimum samples to take category average into account
+    smoothing (int) : smoothing effect to balance categorical average vs prior
+    """
+    def add_noise(series, noise_level):
+        return series * (1 + noise_level * np.random.randn(len(series)))
+    
+    assert len(trn_series) == len(target)
+    assert trn_series.name == tst_series.name
+    temp = pd.concat([trn_series, target], axis=1)
+    # Compute target mean
+    averages = temp.groupby(by=trn_series.name)[target.name].agg(["mean", "count"])
+    # Compute smoothing
+    smoothing = 1 / (1 + np.exp(-(averages["count"] - min_samples_leaf) / smoothing))
+    # Apply average function to all target data
+    prior = target.mean()
+    # The bigger the count the less full_avg is taken into account
+    averages[target.name] = prior * (1 - smoothing) + averages["mean"] * smoothing
+    averages.drop(["mean", "count"], axis=1, inplace=True)
+    # Apply averages to trn and tst series
+    ft_trn_series = pd.merge(
+        trn_series.to_frame(trn_series.name),
+        averages.reset_index().rename(columns={'index': target.name, target.name: 'average'}),
+        on=trn_series.name,
+        how='left')['average'].rename(trn_series.name + '_mean').fillna(prior)
+    # pd.merge does not keep the index so restore it
+    ft_trn_series.index = trn_series.index
+    ft_tst_series = pd.merge(
+        tst_series.to_frame(tst_series.name),
+        averages.reset_index().rename(columns={'index': target.name, target.name: 'average'}),
+        on=tst_series.name,
+        how='left')['average'].rename(trn_series.name + '_mean').fillna(prior)
+    # pd.merge does not keep the index so restore it
+    ft_tst_series.index = tst_series.index
+    
+    return add_noise(ft_trn_series, noise_level), add_noise(ft_tst_series, noise_level)
+    
+    
+# Dealing with Categorical features Way 2    
 def doOneHot(df1, ranges):
     '''
     Ready made column one hot function. Use for categorical features especially when using tree based classifiers.

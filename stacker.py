1c313cbcb59bac277cd407cbb8ba0283c24fdcdd
bsvineethiitg <bhaskaravineeth@gmail.com>
2017-11-23 19:42:31 +0000

    added stacker

---
+++ stacker.py
@@ -0,0 +1,83 @@
+import pandas as pd
+import numpy as np
+
+from sklearn.model_selection import StratifiedKFold
+from sklearn.model_selection import cross_val_score
+
+from xgboost import XGBClassifier
+from lightgbm import LGBMClassifier
+from catboost import CatBoostClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import GradientBoostingClassifier, RandomForestClassifier, ExtraTreesClassifier, AdaBoostClassifier
+
+# Regularized Greedy Forest
+from rgf.sklearn import RGFClassifier     # https://github.com/fukatani/rgf_python
+
+
+'''
+Simple Stacker designed for sklearn wrap classifiers for binary prediction. 
+
+How to use?
+
+First decide on the params for each of the base models. Put them in a dict. 
+Something like:
+
+lgb1 = LGBMClassifier(**params1)
+lgb2 = LGBMClassifier(**params2)
+.
+.
+.
+
+
+top_model = LogisticRegression()
+
+stack = Stacker(n_splits=3,
+        stacker = top_model,
+        base_models = (lgb1, lgb2, lgb3))        
+        
+y_pred = stack.fit_predict(train, target_train, test)  # get the predictions!!
+
+'''
+
+
+class Stacker(object):
+    def __init__(self, n_splits, stacker, base_models):
+        self.n_splits = n_splits
+        self.stacker = stacker
+        self.base_models = base_models
+
+    def fit_predict(self, X, y, T):
+        X = np.array(X)
+        y = np.array(y)
+        T = np.array(T)
+
+        folds = list(StratifiedKFold(n_splits=self.n_splits, shuffle=True, random_state=2016).split(X, y))
+
+        S_train = np.zeros((X.shape[0], len(self.base_models)))
+        S_test = np.zeros((T.shape[0], len(self.base_models)))
+        for i, clf in enumerate(self.base_models):
+
+            S_test_i = np.zeros((T.shape[0], self.n_splits))
+
+            for j, (train_idx, test_idx) in enumerate(folds):
+                X_train = X[train_idx]
+                y_train = y[train_idx]
+                X_holdout = X[test_idx]
+#                y_holdout = y[test_idx]
+
+                print ("Fit %s fold %d" % (str(clf).split('(')[0], j+1))
+                clf.fit(X_train, y_train)
+#                cross_score = cross_val_score(clf, X_train, y_train, cv=3, scoring='roc_auc')
+#                print("    cross_score: %.5f" % (cross_score.mean()))
+                y_pred = clf.predict_proba(X_holdout)[:,1]                
+
+                S_train[test_idx, i] = y_pred
+                S_test_i[:, j] = clf.predict_proba(T)[:,1]
+            S_test[:, i] = S_test_i.mean(axis=1)
+
+        results = cross_val_score(self.stacker, S_train, y, cv=3, scoring='roc_auc')
+        print("Stacker score: %.5f" % (results.mean()))
+
+        self.stacker.fit(S_train, y)
+        res = self.stacker.predict_proba(S_test)[:,1]
+        return res

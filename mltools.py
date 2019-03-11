f818831fba467b727ff0e4e74c2bcd77d3d74a1d
Vineeth S. Bhaskara <bhaskaravineeth@gmail.com>
2019-03-11 19:30:01 -0400

    added a pretty confusion matrix wrapper - plot_confusion_matrix

--- mltools.py
+++ mltools.py
@@ -14,13 +14,131 @@ import cv2
 import random
 from xtune import *
 from sklearn.metrics import roc_auc_score, log_loss
+from sklearn.metrics import confusion_matrix
 import matplotlib.pyplot as plt
 import seaborn as sns
+import itertools
 
 # TODO:
 # Added HM, GM based weights desperateFitter
 
 
+def plot_confusion_matrix(y_true, y_pred, 
+                          target_names_map,
+                          cutoff=None,
+                          title='Confusion matrix',
+                          cmap=None,
+                          normalize=True):
+    """
+    given a sklearn confusion matrix (cm), make a nice plot
+
+    Arguments
+    ---------
+    y_true : array, shape = [n_samples]
+    Ground truth (correct) target values.
+
+    y_pred : array, shape = [n_samples]
+    Estimated targets as returned by a classifier.
+
+    target_names_map: given classification classes mapped to
+                  the class names, for example: {0:'high', 1:'medium', 2:'low'}
+    
+    cutoff:       if cutoff is None, then y_pred is expected to be prediction classes
+                but not probabilities. Specify a cutoff then you can send in probabs for y_pred. 
+                Of course, y_true must always be class labels! Example: {0:0.5, 1:0.3, 2:0.2} this means that
+                if y_pred for class 1 is > 0.3 then it will be classified as class 1. Precedence is 0 to num_classes.
+                For binary cases, use {0:1-p, 1:p} where p is the cutoff you want to be consistent.
+
+    title:        the text to display at the top of the matrix
+
+    cmap:         the gradient of the values displayed from matplotlib.pyplot.cm
+                  see http://matplotlib.org/examples/color/colormaps_reference.html
+                  plt.get_cmap('jet') or plt.cm.Blues
+
+    normalize:    If False, plot the raw numbers
+                  If True, plot the proportions
+
+    Usage
+    -----
+    p=0.1
+    plot_confusion_matrix(df_test['label'].values.tolist(), val_pred, 
+                              target_names_map={0:'survive', 1:'death'},
+                              cutoff={0:1-p, 1:p},
+                              title='Confusion matrix',
+                              cmap=None,
+                              normalize=False)
+
+    Citiation
+    ---------
+    http://scikit-learn.org/stable/auto_examples/model_selection/plot_confusion_matrix.html
+
+    """
+    if cutoff is not None:
+        y_pred_labels = []
+        
+        for pred in y_pred:
+            done = False
+            for i in range(len(target_names_map.keys())):
+                probab = None
+                try: 
+                    probab = pred[i]
+                except:
+                    try:
+                        probab = pred[0]
+                    except:
+                        probab = pred
+
+                if probab > cutoff[i]:
+                    y_pred_labels.append(i)
+                    done=True
+                    break
+            if not done:
+                y_pred_labels.append(i) # predict as the last label if none of the cutoffs match       
+        
+        y_pred = y_pred_labels
+    
+    target_names = target_names_map.values()
+    labels = target_names_map.keys()
+    
+    cm = confusion_matrix(y_true, y_pred, labels=labels)
+    
+    accuracy = np.trace(cm) / float(np.sum(cm))
+    misclass = 1 - accuracy
+
+    if cmap is None:
+        cmap = plt.get_cmap('Blues')
+
+    plt.figure(figsize=(8, 6))
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+
+    if target_names is not None:
+        tick_marks = np.arange(len(target_names))
+        plt.xticks(tick_marks, target_names, rotation=45)
+        plt.yticks(tick_marks, target_names)
+
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+
+
+    thresh = cm.max() / 1.5 if normalize else cm.max() / 2
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        if normalize:
+            plt.text(j, i, "{:0.4f}".format(cm[i, j]),
+                     horizontalalignment="center",
+                     color="white" if cm[i, j] > thresh else "black")
+        else:
+            plt.text(j, i, "{:,}".format(cm[i, j]),
+                     horizontalalignment="center",
+                     color="white" if cm[i, j] > thresh else "black")
+
+
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label\naccuracy={:0.4f}; misclass={:0.4f}'.format(accuracy, misclass))
+    plt.show()
+    
 def normalizedf(df):
     '''
     Normalizes dataframe columns to 1. Send in slice of those columns
